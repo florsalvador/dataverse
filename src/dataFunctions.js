c4d56d2fc1af11531dd5f6b1ee324aa9d8e86b27
Adri <adrianastef1425@gmail.com>
2024-03-13 16:39:55 -0500

    cambios hasta sortOrder

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,13 +1,32 @@
+/*
 export const filterData = (data, filterBy, value) => {
   const dataFiltered = data.filter(cat => {
     if (cat["facts"][filterBy] === value) {
       return cat;
-    } else if (cat["extraInfo"] && cat["extraInfo"][filterBy] && cat["extraInfo"][filterBy][value]) {
+    } else if (cat["extraInfo"][filterBy] && cat["extraInfo"][filterBy][value]) {
       return cat;
     }
+
   });
   return dataFiltered;
 };
+*/
+
+
+export const filterData = (data, filterBy, value) => {
+  return data.filter(cat => cat["facts"][filterBy] === value)
+};
+
+
+export const filterData2 = (data, filterBy, value) => {
+  return data.filter(cat => cat["extraInfo"][filterBy][value]) 
+};
+
+
+
+
+
+//----------------------------------
 
 // FUNCION FILTERDATA PELAJE Y PERSONALIDAD USANDO OR PERO NO FUNCIONA
 // export const filterData = (data, filterBy, value) => {
@@ -33,3 +52,39 @@ export const filterData = (data, filterBy, value) => {
 //     return descendente;
 //   } else return data;
 // };
+
+//---------------------------------------
+
+
+//cambio adriana // orden A-Z y de Z-A
+export const sortData = (data, sortBy, sortOrder) => {
+  const data1 = data;
+  data1.sort((a, b) =>{
+    if(sortOrder === "asc"){
+      return (a[sortBy] > b[sortBy]) ? 1 : -1; //codigo ternario si es true devuelve 1 si es false devuelve -1
+    }else if(sortOrder === "desc"){
+      return (a[sortBy] > b[sortBy]) ? -1 : 1;
+    }else return 0;
+  });
+
+  return data1;
+
+};
+
+
+
+/*
+//precio cachorro
+export const sortData = (data, sortBy, sortOrder) => {
+  const data1 = data;
+  data1.sort((a, b) =>{
+    if(sortOrder === "desc"){
+      return (a["facts"][sortBy] < b["facts"][sortBy]) ? 1 : -1; //codigo ternario si es true devuelve 1 si es false devuelve -1
+    }else if(sortOrder === "asc"){
+      return (a["facts"][sortBy] > b["facts"][sortBy]) ? 1 : -1;
+    }else return 0;
+  });
+
+  return data1;
+}
+*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // import { example } from './dataFunctions.js';
 import { filterData } from './dataFunctions.js';
-// import { sortData } from './dataFunctions.js';
+import { filterData2 } from './dataFunctions.js';
+import { sortData } from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
@@ -12,24 +13,27 @@ root.appendChild(renderItems(data));
 // evento para select pelaje
 const selectPelaje = document.getElementById("pelajeGato");
 selectPelaje.addEventListener("change", function() {
+
+  root.innerHTML = "";
+
   if (selectPelaje.value === "pelo-corto") {
-    root.innerHTML = "";
+
     const peloCorto = filterData(data, "pelajeGato", "Pelo corto");
     root.appendChild(renderItems(peloCorto));
   } else if (selectPelaje.value === "pelo-semilargo") {
-    root.innerHTML = "";
+  
     const peloSemilargo = filterData(data, "pelajeGato", "Pelo semilargo");
     root.appendChild(renderItems(peloSemilargo));
   } else if (selectPelaje.value === "pelo-largo") {
-    root.innerHTML = "";
+    
     const peloLargo = filterData(data, "pelajeGato", "Pelo largo");
     root.appendChild(renderItems(peloLargo));
   } else if (selectPelaje.value === "sin-pelo") {
-    root.innerHTML = "";
+    
     const sinPelo = filterData(data, "pelajeGato", "Sin pelo");
     root.appendChild(renderItems(sinPelo));
   } else {
-    root.innerHTML = "";
+    
     root.appendChild(renderItems(data));
   }
 });
@@ -37,32 +41,35 @@ selectPelaje.addEventListener("change", function() {
 // evento para select personalidad
 const selectPersonalidad = document.getElementById("personalidad");
 selectPersonalidad.addEventListener("change", function() {
+
+  root.innerHTML = "";
+
   if (selectPersonalidad.value === "tranquilo") {
-    root.innerHTML = "";
-    const tranquilo = filterData(data, "personalidad", "tranquilo");
+    
+    const tranquilo = filterData2(data, "personalidad", "tranquilo");
     root.appendChild(renderItems(tranquilo));
   } else if (selectPersonalidad.value === "carinoso") {
-    root.innerHTML = "";
-    const carinoso = filterData(data, "personalidad", "carinoso");
+    
+    const carinoso = filterData2(data, "personalidad", "carinoso");
     root.appendChild(renderItems(carinoso));
   } else if (selectPersonalidad.value === "sociable") {
-    root.innerHTML = "";
-    const sociable = filterData(data, "personalidad", "sociable");
+    
+    const sociable = filterData2(data, "personalidad", "sociable");
     root.appendChild(renderItems(sociable));
   } else if (selectPersonalidad.value === "energico") {
-    root.innerHTML = "";
-    const energico = filterData(data, "personalidad", "energico");
+    
+    const energico = filterData2(data, "personalidad", "energico");
     root.appendChild(renderItems(energico));
   } else if (selectPersonalidad.value === "jugueton") {
-    root.innerHTML = "";
-    const jugueton = filterData(data, "personalidad", "jugueton");
+    
+    const jugueton = filterData2(data, "personalidad", "jugueton");
     root.appendChild(renderItems(jugueton));
   } else {
-    root.innerHTML = "";
+    
     root.appendChild(renderItems(data));
   }
 })
-
+//_-------------------------------
 // evento para ordenar
 // const ordenar = document.getElementById("ordenar");
 // ordenar.addEventListener("change", function() {
@@ -80,4 +87,29 @@ selectPersonalidad.addEventListener("change", function() {
 //   }
 // })
 
+//--------------------------------------
+
 //cambio de la rama feature-newbranchtest
+
+//cambio adriana
+// evento para ordenar de az
+const ordenar = document.getElementById("orden");
+ordenar.addEventListener("change", function() {
+
+  root.innerHTML = "";
+
+  if (ordenar.value === "asc") {
+    
+    const ascendente = sortData(data, "id", "asc");
+    root.appendChild(renderItems(ascendente));
+  } else if (ordenar.value === "desc") {
+    
+    const descendente = sortData(data, "id", "desc");
+    root.appendChild(renderItems(descendente));
+  
+  }
+});
+
+//para precio cachorro cambiar "id" por "precioCachorro"
+
+//console.log(sortData(data,"precioCachorro","asc"));
