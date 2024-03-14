cf98afea519a5c4286eadaa924c7dc0e9896064b
florsalvador <florsalvador219@gmail.com>
2024-03-14 13:09:14 -0500

    Agrega datos a las tarjetas

--- src/main.js
+++ src/main.js
@@ -6,88 +6,123 @@ import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
 // muestra los datos en index
-const root = document.getElementById("root");
+const root = document.querySelector("#root");
 document.querySelector("main").appendChild(root);
 root.appendChild(renderItems(data));
 
 // evento para select pelaje
-const selectPelaje = document.getElementById("pelajeGato");
-selectPelaje.addEventListener("change", function() {
-
+const selectPelaje = document.querySelector("#pelajeGato");
+selectPelaje.addEventListener("change", function () {
   root.innerHTML = "";
-
+  selectPersonalidad.selectedIndex = 0; // muestra opción "Todos" en filtro de personalidad
   if (selectPelaje.value === "pelo-corto") {
-
     const peloCorto = filterData(data, "pelajeGato", "Pelo corto");
     root.appendChild(renderItems(peloCorto));
   } else if (selectPelaje.value === "pelo-semilargo") {
-  
     const peloSemilargo = filterData(data, "pelajeGato", "Pelo semilargo");
     root.appendChild(renderItems(peloSemilargo));
   } else if (selectPelaje.value === "pelo-largo") {
-    
     const peloLargo = filterData(data, "pelajeGato", "Pelo largo");
     root.appendChild(renderItems(peloLargo));
   } else if (selectPelaje.value === "sin-pelo") {
-    
     const sinPelo = filterData(data, "pelajeGato", "Sin pelo");
     root.appendChild(renderItems(sinPelo));
   } else {
-    
     root.appendChild(renderItems(data));
   }
 });
 
+// evento para select pelaje con funcion afuera
+// const selectPelaje = document.querySelector("#pelajeGato");
+
+// const eventPelaje = () => {
+//   root.innerHTML = "";
+//   selectPersonalidad.selectedIndex = 0; // muestra opción "Todos" en filtro de personalidad
+//   if (selectPelaje.value === "pelo-corto") {
+//     const peloCorto = filterData(data, "pelajeGato", "Pelo corto");
+//     root.appendChild(renderItems(peloCorto));
+//   } else if (selectPelaje.value === "pelo-semilargo") {
+//     const peloSemilargo = filterData(data, "pelajeGato", "Pelo semilargo");
+//     root.appendChild(renderItems(peloSemilargo));
+//   } else if (selectPelaje.value === "pelo-largo") {
+//     const peloLargo = filterData(data, "pelajeGato", "Pelo largo");
+//     root.appendChild(renderItems(peloLargo));
+//   } else if (selectPelaje.value === "sin-pelo") {
+//     const sinPelo = filterData(data, "pelajeGato", "Sin pelo");
+//     root.appendChild(renderItems(sinPelo));
+//   } else {
+//     root.appendChild(renderItems(data));
+//   }
+// }
+
+// selectPelaje.addEventListener("change", eventPelaje);
+
+// const pelajeSelected = 
+
 // evento para select personalidad
-const selectPersonalidad = document.getElementById("personalidad");
-selectPersonalidad.addEventListener("change", function() {
-
+const selectPersonalidad = document.querySelector("#personalidad");
+selectPersonalidad.addEventListener("change", function () {
   root.innerHTML = "";
-
+  selectPelaje.selectedIndex = 0; // muestra opción "Todos" en filtro de pelaje
   if (selectPersonalidad.value === "tranquilo") {
-    
     const tranquilo = filterDataObj(data, "personalidad", "tranquilo");
     root.appendChild(renderItems(tranquilo));
   } else if (selectPersonalidad.value === "carinoso") {
-    
     const carinoso = filterDataObj(data, "personalidad", "carinoso");
     root.appendChild(renderItems(carinoso));
   } else if (selectPersonalidad.value === "sociable") {
-    
     const sociable = filterDataObj(data, "personalidad", "sociable");
     root.appendChild(renderItems(sociable));
   } else if (selectPersonalidad.value === "energico") {
-    
     const energico = filterDataObj(data, "personalidad", "energico");
     root.appendChild(renderItems(energico));
   } else if (selectPersonalidad.value === "jugueton") {
-    
     const jugueton = filterDataObj(data, "personalidad", "jugueton");
     root.appendChild(renderItems(jugueton));
   } else {
-    
     root.appendChild(renderItems(data));
   }
-})
+});
 
 // evento para ordenar de az
-const ordenar = document.getElementById("orden");
-ordenar.addEventListener("change", function() {
-
+const ordenar = document.querySelector("#orden");
+ordenar.addEventListener("change", function () {
   root.innerHTML = "";
-
   if (ordenar.value === "A-Z") {
-    
     const ascendente = sortData(data, "id", "asc");
     root.appendChild(renderItems(ascendente));
   } else if (ordenar.value === "Z-A") {
-    
     const descendente = sortData(data, "id", "desc");
     root.appendChild(renderItems(descendente));
-  
   } 
+  // else if (ordenar.value === "sin-orden") {
+  //   root.appendChild(renderItems(data));
+  // }
 });
 
-//para precio cachorro cambiar "id" por "precioCachorro"
+// evento para ordenar todo
+// const ordenar = document.querySelector("#orden");
+// ordenar.addEventListener("change", function () {
+//   root.innerHTML = "";
+//   if (selectPelaje.value !== "todos") {
+//     if (ordenar.value === "A-Z") {
+//       const ascendente = sortData(filterData(data, "pelajeGato", "Pelo corto"), "id", "asc");
+//       root.appendChild(renderItems(ascendente));
+//     } else if (ordenar.value === "Z-A") {
+//       const descendente = sortData(eventPelaje, "id", "desc");
+//       root.appendChild(renderItems(descendente));
+//     }
+//   } else if (ordenar.value === "A-Z") {
+//     const ascendente = sortData(data, "id", "asc");
+//     root.appendChild(renderItems(ascendente));
+//   } else if (ordenar.value === "Z-A") {
+//     const descendente = sortData(data, "id", "desc");
+//     root.appendChild(renderItems(descendente));
+//   }
+// });
 
-//console.log(sortData(data,"precioCachorro","asc"));
+// Al cargar la página, establecer el valor del select pelaje como "todos"
+window.addEventListener("load", function () {
+  selectPelaje.value = "todos";
+  selectPersonalidad.value = "todos";
+});
--- src/view.js
+++ src/view.js
@@ -3,19 +3,31 @@ export const renderItems = (data) => {
   const newUl = document.createElement("ul");
   //crear la etiqueta ul  
   data.forEach(onlyCat => {
-    const img = document.createElement("img");//crear la etiqueta img
-    img.src = onlyCat.imageUrl;//insertar la imagen de la data en la etiqueta img
+    // const img = document.createElement("img");//crear la etiqueta img
+    // img.src = onlyCat.imageUrl;//insertar la imagen de la data en la etiqueta img
     const newLi = document.createElement("li");//crear la etiqueta li para un gato
-    newLi.appendChild(img);//inserta la etiqueta img en la etiqueta li de ese gato
-    const h2 = document.createElement("h2");//crear la etiqueta h2
-    const nameCat = document.createTextNode(onlyCat.name);//crea un nuevo texto, este texto sera el nombre de cada gato
-    //document.createTextNode(JSON.stringify(data[i]));
-    h2.appendChild(nameCat);//inserta el nombre de cada gato a la etiqueta h2
-    newLi.appendChild(h2); //inserta la etiqueta h2 en la etiqueta li, hace esto para cada gato en la iteración
-    const p = document.createElement("p");//crea la etiqueta p
-    const shortDesc = document.createTextNode(onlyCat.shortDescription);//crea un nuevo texto, este texto sera la shortDescription de cada gato
-    p.appendChild(shortDesc);//inserta la shortdescription de cada gato en la etiqueta p
-    newLi.appendChild(p);//inserta la etiqueta p en la etiqueta li
+    // newLi.appendChild(img);//inserta la etiqueta img en la etiqueta li de ese gato
+    // const h2 = document.createElement("h2");//crear la etiqueta h2
+    // const nameCat = document.createTextNode(onlyCat.name);//crea un nuevo texto, este texto sera el nombre de cada gato
+    // //document.createTextNode(JSON.stringify(data[i]));
+    // h2.appendChild(nameCat);//inserta el nombre de cada gato a la etiqueta h2
+    // newLi.appendChild(h2); //inserta la etiqueta h2 en la etiqueta li, hace esto para cada gato en la iteración
+    // const p = document.createElement("p");//crea la etiqueta p
+    // const shortDesc = document.createTextNode(onlyCat.shortDescription);//crea un nuevo texto, este texto sera la shortDescription de cada gato
+    // p.appendChild(shortDesc);//inserta la shortdescription de cada gato en la etiqueta p
+    // newLi.appendChild(p);//inserta la etiqueta p en la etiqueta li
+
+    newLi.innerHTML = `<li itemscope itemtype="RazasDeGatos">
+    <img src="${onlyCat.imageUrl}" alt="${onlyCat.id}">
+    <h2 itemprop="name">${onlyCat.name}</h2>
+    <p itemprop="shortDescription">${onlyCat.shortDescription}</p>
+    <p>Tamaño: <span itemprop="tamanoGato">${onlyCat.facts["tamanoGato"]}</span></p>
+    <p>Pelaje: <span itemprop="pelajeGato">${onlyCat.facts["pelajeGato"]}</span></p>
+    <p>Esperanza de vida: <span itemprop="esperanzaMax">${onlyCat.facts["esperanzaMax"]}</span></p>
+    <p>Precio: <span itemprop="precioCachorro">${onlyCat.facts["precioCachorro"]}</span></p>
+    <button id="ver-mas" class="botonVer">Ver más</button>
+  </li>`
+
     newUl.appendChild(newLi);//inserta la etiqueta li en la etiqueta ul
   });
   return newUl;
